9ca623fca0ec887b4a48f012ef5ed192eba260d4
vidhi jain <117736922+vi3003@users.noreply.github.com>
2023-03-17 16:07:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,20 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+  let prices=document.querySelectorAll(".price");
+let totalPrice=0;
+  for(let i=0;i<prices.length;i++){
+    totalPrice+=Number(prices[i].innerHTML);
+  }
+  console.log(totalPrice);
+  let row=document.createElement("tr");
+//   let td1=document.createElement("td");
+//   td1.textContent="Total Price";
+  let td2=document.createElement("td");
+  td2.textContent=totalPrice;
+  td2.setAttribute("id","ans");
+  row.append(td2);
+  document.querySelector("table").append(row);
 };
 
 getSumBtn.addEventListener("click", getSum);
